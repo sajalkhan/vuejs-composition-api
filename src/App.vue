dbc9b34270d657fb63fc3f13bdf0a77498433a7e
sajalkhan <sajalhossen@yahoo.com>
2022-10-23 21:00:43 +0600

    configure vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,11 @@
 <template>
-  <section class="p-main">
-    <div class="p-main__container">
-      <Suspense>
-        <template #default>
-          <TimeLineDetails />
-        </template>
-        <template #fallback>
-          <Spinner />
-        </template>
-      </Suspense>
-    </div>
-  </section>
+  <router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import TimeLineDetails from "@/components/organisms/timeline-details/index.vue";
-import Spinner from "@/components/atoms/spinner/index.vue";
 
 export default defineComponent({
   name: "App",
-  components: {
-    TimeLineDetails,
-    Spinner,
-  },
 });
 </script>
-
-<style lang="scss">
-.p-main {
-  height: 100%;
-  width: 100%;
-
-  &__container {
-    padding: 50px 100px;
-  }
-}
-</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="p-home__container">
+    <Suspense>
+      <template #default>
+        <TimeLineDetails />
+      </template>
+      <template #fallback>
+        <Spinner />
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import TimeLineDetails from "@/components/organisms/timeline-details/index.vue";
+import Spinner from "@/components/atoms/spinner/index.vue";
+
+export default defineComponent({
+  name: "HomePage",
+  components: {
+    TimeLineDetails,
+    Spinner,
+  },
+});
+</script>
+
+<style lang="scss">
+.p-home {
+  height: 100%;
+  width: 100%;
+
+  &__container {
+    padding: 50px 100px;
+  }
+}
+</style>
